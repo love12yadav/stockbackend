7778e3804de7233df561da4ede4cfffaf2242f5c
Love yadav <155807335+love12yadav@users.noreply.github.com>
2025-07-08 12:27:37 +0530

    Update CorsConfig.java

--- src/main/java/com/Stock/demo/CorsConfig.java
+++ src/main/java/com/Stock/demo/CorsConfig.java
@@ -1,17 +1,19 @@
 package com.Stock.demo;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // allow all paths
-                .allowedOrigins("http://localhost:5173") // or your frontend port
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "http://localhost:5173",               // local dev
+                    "https://dostock-psi.vercel.app"       // deployed frontend
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
